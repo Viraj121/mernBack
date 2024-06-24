35f4ea22fb2e8af8f12aec6df11c47bbe5d4174c
Viraj Gavas <gavasviraj@gmail.com>
2024-06-24 22:15:32 +0530

    new

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const upload = multer({ storage: storage });
 app.post("/upload", upload.single("product"), (req, res) => {
   res.json({
     success: 1,
-    image_url: `https://mernback-eezd.onrender.com/images/${req.file.filename}`,
+    image_url: `https://ecommerce-app-backend-5hrm.onrender.com/images/${req.file.filename}`,
   });
 });
 app.use("/images", express.static("upload/images"));
